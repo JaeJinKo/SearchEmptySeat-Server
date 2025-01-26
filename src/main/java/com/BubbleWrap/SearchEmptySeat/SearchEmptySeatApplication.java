ce20a7a48bd0d6d9b8b42e34b839a096f06b2a25
JaeJinKo <er12345600@gmail.com>
2025-01-26 16:22:03 +0900

    init

---
+++ src/main/java/com/BubbleWrap/SearchEmptySeat/SearchEmptySeatApplication.java
@@ -0,0 +1,13 @@
+package com.BubbleWrap.SearchEmptySeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchEmptySeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchEmptySeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BubbleWrap/SearchEmptySeat/SearchEmptySeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BubbleWrap.SearchEmptySeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchEmptySeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
